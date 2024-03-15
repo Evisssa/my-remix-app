ea9cac2183b9719c3054b95c73ac5b722505bffd
Evisssa <evisa.skarra@gmail.com>
2024-03-15 23:33:00 +0100

    quick annotation to get type inference about our data with typeof loader

--- app/root.tsx
+++ app/root.tsx
@@ -1,20 +1,34 @@
-import type { LinksFunction } from "@remix-run/node";
-// existing imports
-
+import type {LinksFunction} from "@remix-run/node";
 import appStylesHref from "./app.css?url";
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: appStylesHref },
+export const links: LinksFunction = () =>[
+  {rel:"StyleSheet", href: appStylesHref},
 ];
+
+import {json} from "@remix-run/node";
 import {
+  Form,
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+
+import { getContacts } from "./data";
+
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
+
+export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -24,14 +38,61 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
+                id="q"
+                aria-label="Search contacts"
+                placeholder="Search"
+                type="search"
+                name="q"
+              />
+              <div id="search-spinner" aria-hidden hidden={true} />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+
+          <nav>
+          {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </nav>
+          
+          
+        </div>
+
+        
+        <div id="Detail">
+            <Outlet/>
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
