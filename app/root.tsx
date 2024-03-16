229c57974bb6d293c8d4d28d8450242704b90a1a
Evisssa <evisa.skarra@gmail.com>
2024-03-16 23:37:29 +0100

    Adding function on mutating data

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,12 @@ import {
 } from "@remix-run/react";
 
 
-import { getContacts } from "./data";
+import { createEmptyContact,getContacts } from "./data";
+
+export const action  = async ()=>{
+  const contact = await createEmptyContact();
+  return json({contact});
+};
 
 
 export const loader = async () => {
